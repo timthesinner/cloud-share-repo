4a29e89be0265a900075fca2e2d19973dcf80add
TimTheSinner <timthesinner@gmail.com>
2014-01-08 22:23:11 -0700

    Finished gradle build:  - Can now prepare, archive, and release plugins  - Can now prepare, archive, and release plugins individually
    Added *-src and .gradle ignores to addons_xml_generator.py

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -56,7 +56,7 @@ class Generator:
         for addon in addons:
             try:
                 # skip any file or .svn folder or .git folder
-                if ( not os.path.isdir( addon ) or addon == ".svn" or addon == ".git" ): continue
+                if ( not os.path.isdir( addon ) or addon == ".svn" or addon == ".git" or addon == ".gradle" or addon.endswith("-src") ): continue
                 # create path
                 _path = os.path.join( addon, "addon.xml" )
                 # split lines for stripping
